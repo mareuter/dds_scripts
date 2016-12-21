f497e565b774ade0f2f830ce180c4f1169e2db23
Michael Reuter <mareuter@lsst.org>
2016-12-21 12:53:08 -0700

    First cuts at SOCS/Scheduler DDS testers.

---
+++ scheduler_tester.py
@@ -0,0 +1,100 @@
+from SALPY_scheduler import *
+import time
+
+def recv_topic(function, topic, message_success, message_failure):
+    waitconfig = True
+    lastconfigtime = time.time()
+    while waitconfig:
+        scode = function(topic)
+        if scode == 0:
+            print(message_success)
+            lastconfigtime = time.time()
+            waitconfig = False
+        else:
+            tf = time.time()
+            if (tf - lastconfigtime > 10.0):
+                print(message_failure)
+                waitconfig = False
+
+sal = SAL_scheduler()
+sal.setDebugLevel(0)
+
+# Initialize all topics
+topic_schedulerConfig = scheduler_schedulerConfigC()
+topic_driverConfig = scheduler_driverConfigC()
+topic_obsSiteConfig = scheduler_obsSiteConfigC()
+topic_telescopeConfig = scheduler_telescopeConfigC()
+topic_domeConfig = scheduler_domeConfigC()
+topic_rotatorConfig = scheduler_rotatorConfigC()
+topic_cameraConfig = scheduler_cameraConfigC()
+topic_slewConfig = scheduler_slewConfigC()
+topic_opticsConfig = scheduler_opticsLoopCorrConfigC()
+topic_parkConfig = scheduler_parkConfigC()
+topic_areaDistPropConfig = scheduler_generalPropConfigC()
+topicTime = scheduler_timeHandlerC()
+topicObservatoryState = scheduler_observatoryStateC()
+topic_cloud = scheduler_cloudC()
+topic_seeing = scheduler_seeingC()
+topicObservation = scheduler_observationC()
+topicField = scheduler_fieldC()
+topicTarget = scheduler_targetC()
+topicFilterSwap = scheduler_filterSwapC()
+topicInterestedProposal = scheduler_interestedProposalC()
+
+# Initialize all subs and pubs
+
+sal.salTelemetrySub("scheduler_schedulerConfig")
+sal.salTelemetrySub("scheduler_driverConfig")
+sal.salTelemetrySub("scheduler_obsSiteConfig")
+sal.salTelemetrySub("scheduler_telescopeConfig")
+sal.salTelemetrySub("scheduler_domeConfig")
+sal.salTelemetrySub("scheduler_rotatorConfig")
+sal.salTelemetrySub("scheduler_cameraConfig")
+sal.salTelemetrySub("scheduler_slewConfig")
+sal.salTelemetrySub("scheduler_opticsLoopCorrConfig")
+sal.salTelemetrySub("scheduler_parkConfig")
+sal.salTelemetrySub("scheduler_generalPropConfig")
+sal.salTelemetrySub("scheduler_timeHandler")
+sal.salTelemetrySub("scheduler_observatoryState")
+sal.salTelemetrySub("scheduler_cloud")
+sal.salTelemetrySub("scheduler_seeing")
+sal.salTelemetrySub("scheduler_observation")
+sal.salTelemetrySub("scheduler_field")
+sal.salTelemetryPub("scheduler_target")
+sal.salTelemetryPub("scheduler_filterSwap")
+sal.salTelemetryPub("scheduler_interestedProposal")
+
+print("Scheduler Ready")
+
+recv_topic(sal.getNextSample_schedulerConfig, topic_schedulerConfig,
+           "scheduler Config received", "scheduler Config timeout")
+recv_topic(sal.getNextSample_driverConfig, topic_driverConfig,
+           "driver Config received", "driver Config timeout")
+recv_topic(sal.getNextSample_telescopeConfig, topic_telescopeConfig,
+           "telescope Config received", "telescope Config timeout")
+recv_topic(sal.getNextSample_domeConfig, topic_domeConfig,
+           "dome Config received", "dome Config timeout")
+recv_topic(sal.getNextSample_rotatorConfig, topic_rotatorConfig,
+           "rotator Config received", "rotator Config timeout")
+recv_topic(sal.getNextSample_cameraConfig, topic_cameraConfig,
+           "camera Config received", "camera Config timeout")
+recv_topic(sal.getNextSample_slewConfig, topic_slewConfig,
+           "slew Config received", "slew Config timeout")
+recv_topic(sal.getNextSample_opticsLoopCorrConfig, topic_opticsLoopCorrConfig,
+           "opticsLoopCorr Config received", "opticsLoopCorr Config timeout")
+recv_topic(sal.getNextSample_parkConfig, topic_parkConfig,
+           "park Config received", "park Config timeout")
+recv_topic(sal.getNextSample_generalPropConfig, topic_generalPropConfig,
+           "generalProp Config received", "generalProp Config timeout")
+
+topicField.ID = -1
+sal.putSample_field(topicField)
+for i in range(5292):
+    topicField.ID = i + 1
+    sal.putSample_field(topicField)
+topicField.ID = -1
+sal.putSample_field(topicField)
+
+print("Fields transferred")
+
+sal.salShutdown()
---
+++ socs_tester.py
@@ -0,0 +1,100 @@
+from SALPY_scheduler import *
+import time
+
+sal = SAL_scheduler()
+sal.setDebugLevel(0)
+
+def send_topic(func, topic, message_success, message_failure):
+    rcode = func(topic)
+    if rcode == 0:
+        print(message_success)
+    else:
+        print(message_failure)
+
+
+# Initialize all topics
+topic_schedulerConfig = scheduler_schedulerConfigC()
+topic_driverConfig = scheduler_driverConfigC()
+topic_obsSiteConfig = scheduler_obsSiteConfigC()
+topic_telescopeConfig = scheduler_telescopeConfigC()
+topic_domeConfig = scheduler_domeConfigC()
+topic_rotatorConfig = scheduler_rotatorConfigC()
+topic_cameraConfig = scheduler_cameraConfigC()
+topic_slewConfig = scheduler_slewConfigC()
+topic_opticsConfig = scheduler_opticsLoopCorrConfigC()
+topic_parkConfig = scheduler_parkConfigC()
+topic_generalPropConfig = scheduler_generalPropConfigC()
+topicTime = scheduler_timeHandlerC()
+topicObservatoryState = scheduler_observatoryStateC()
+topic_cloud = scheduler_cloudC()
+topic_seeing = scheduler_seeingC()
+topicObservation = scheduler_observationC()
+topicField = scheduler_fieldC()
+topicTarget = scheduler_targetC()
+topicFilterSwap = scheduler_filterSwapC()
+topicInterestedProposal = scheduler_interestedProposalC()
+
+# Initialize all subs and pubs
+
+sal.salTelemetryPub("scheduler_schedulerConfig")
+sal.salTelemetryPub("scheduler_driverConfig")
+sal.salTelemetryPub("scheduler_obsSiteConfig")
+sal.salTelemetryPub("scheduler_telescopeConfig")
+sal.salTelemetryPub("scheduler_domeConfig")
+sal.salTelemetryPub("scheduler_rotatorConfig")
+sal.salTelemetryPub("scheduler_cameraConfig")
+sal.salTelemetryPub("scheduler_slewConfig")
+sal.salTelemetryPub("scheduler_opticsLoopCorrConfig")
+sal.salTelemetryPub("scheduler_parkConfig")
+sal.salTelemetryPub("scheduler_generalPropConfig")
+sal.salTelemetryPub("scheduler_timeHandler")
+sal.salTelemetryPub("scheduler_observatoryState")
+sal.salTelemetryPub("scheduler_cloud")
+sal.salTelemetryPub("scheduler_seeing")
+sal.salTelemetryPub("scheduler_observation")
+sal.salTelemetrySub("scheduler_field")
+sal.salTelemetrySub("scheduler_target")
+sal.salTelemetrySub("scheduler_filterSwap")
+sal.salTelemetrySub("scheduler_interestedProposal")
+
+print("SOCS Ready")
+
+sal.putSample_schedulerConfig(topic_schedulerConfig)
+sal.putSample_driverConfig(topic_driverConfig)
+sal.putSample_obsSiteConfig(topic_obsSiteConfig)
+sal.putSample_telescopeConfig(topic_telescopeConfig)
+sal.putSample_domeConfig(topic_domeConfig)
+sal.putSample_rotatorConfig(topic_rotatorConfig)
+sal.putSample_cameraConfig(topic_cameraConfig)
+sal.putSample_slewConfig(topic_slewConfig)
+sal.putSample_opticsLoopCorrConfig(topic_opticsLoopCorrConfig)
+sal.putSample_parkConfig(topic_parkConfig)
+
+for i in range(4):
+    topic_generalPropConfig.prop_id = i + 1
+    sal.putSample_generalPropConfig(topic_generalPropConfig)
+
+print("Configuration sent")
+print("Retrieving fields")
+
+field_set = []
+fields_from_dds = 0
+end_fields = False
+while True:
+    rcode = sal.getNextSample_field(topicField)
+    if field.ID == 0:
+        continue
+    if rcode == 0 and field.ID == -1:
+        if end_fields:
+            break
+        else:
+            end_fields = True
+            continue
+    field_set.append((field.ID, field.fov, field.ra, field.dec,
+                      field.gl, field.gb, field.el, field.eb))
+    fields_from_dds += 1
+    time.sleep(0.00001)
+
+print("Retrieved {} fields".format(fields_from_dds))
+
+sal.salShutdown()
