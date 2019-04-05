50889872f18e5252db4335d65a252973c7e18af0
Michael Reuter <mareuternh@gmail.com>
2019-04-05 09:17:29 -0700

    First shot at a state transition script.

---
+++ state_transition.py
@@ -0,0 +1,23 @@
+import asyncio
+import sys
+
+from lsst.ts.salobj import Remote
+import SALPY_ATHeaderService
+
+async def transition(state):
+    r = Remote(SALPY_ATHeaderService, None)
+    print(f"Command to {state}")
+    cmd_attr = getattr(r, f"cmd_{state}")
+    cmd_data = cmd_attr.DataType()
+    if state == 'start':
+        cmd_data.settingsToApply = 'default'
+
+    return await cmd_attr.start(cmd_data, timeout=600)
+
+def main(state):
+    loop = asyncio.get_event_loop()
+    print(loop.run_until_complete(transition(state)))
+
+if __name__ == '__main__':
+    state = sys.argv[1]
+    main(state)
