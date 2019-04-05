b82d950bd05ff5bbccfc3363f09eef6f43f7e4b4
Michael Reuter <mareuternh@gmail.com>
2019-04-05 09:15:05 -0700

    Whitespace fix for quick heartbeat script.

--- quick_heartbeat.py
+++ quick_heartbeat.py
@@ -4,4 +4,4 @@ import asyncio
 r = Remote(SALPY_Test, 1)
 
 while True:
-     print(asyncio.get_event_loop().run_until_complete(r.evt_heartbeat.next(flush=False)))
+    print(asyncio.get_event_loop().run_until_complete(r.evt_heartbeat.next(flush=False)))
