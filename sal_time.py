97c1f7261915b67d83756a17c87e9207c9755719
Michael Reuter <mareuter@lsst.org>
2019-10-11 16:51:26 -0700

    Fix sal_time script.

--- sal_time.py
+++ sal_time.py
@@ -5,7 +5,6 @@ import SALPY_ATHeaderService
 mgr = SALPY_ATHeaderService.SAL_ATHeaderService()
 now_sys = time.time()
 now_sal = mgr.getCurrentTime()
-print(time.time())
 print(f"System time: {now_sys}")
 print(f"SAL time: {now_sal}") 
 delta_t = now_sal - now_sys
