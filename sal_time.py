455392c2645902965a7ec996c74f700e9befc08e
Michael Reuter <mareuter@lsst.org>
2019-10-11 14:54:41 -0700

    Add script for time check.

---
+++ sal_time.py
@@ -0,0 +1,12 @@
+import time
+
+import SALPY_ATHeaderService
+
+mgr = SALPY_ATHeaderService.SAL_ATHeaderService()
+now_sys = time.time()
+now_sal = mgr.getCurrentTime()
+print(time.time())
+print(f"System time: {now_sys}")
+print(f"SAL time: {now_sal}") 
+delta_t = now_sal - now_sys
+print(f"Delta T: {delta_t}")
